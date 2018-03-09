cdb1b518873ab59a3bf643666cdbea6c0a0b3aad
Alexander Gazarov <unrealrider@gmail.com>
2018-03-09 20:05:08 +0300

    Fixed incorrect parsing of the no new line marker

--- src/patch_editor/test_data.rs
+++ src/patch_editor/test_data.rs
@@ -28,6 +28,19 @@ new file mode 100644
 index 0000000..e69de29
 "#;
 
+pub const PATCH_DATA_NO_NEW_LINES_HEADER: &[u8] = br"diff --git a/Test file 2.txt b/Test file 2.txt
+index 60c340c..ec6c4de 100644
+--- a/Test file 2.txt
++++ b/Test file 2.txt
+";
+pub const PATCH_DATA_NO_NEW_LINES_HUNK_HEADER: &[u8] = br"@@ -1 +1 @@
+";
+pub const PATCH_DATA_NO_NEW_LINES_HUNK_CONTENTS: &[u8] = br"-This is the second test file - modified
+\ No newline at end of file
++This is the second test file
+\ No newline at end of file
+";
+
 macro_rules! vec_from_slices {
 	($($x:expr), *) => {
 		{
@@ -52,6 +65,12 @@ lazy_static! {
 			&**PATCH_DATA_HUNK_1,
 			PATCH_DATA_HUNK_2_OVERLAPPING_HEADER,
 			PATCH_DATA_HUNK_2_CONTENTS];
+
+	pub static ref PATCH_DATA_NO_NEW_LINES: Vec<u8> = vec_from_slices![
+		PATCH_DATA_NO_NEW_LINES_HEADER,
+		PATCH_DATA_NO_NEW_LINES_HUNK_HEADER,
+		PATCH_DATA_NO_NEW_LINES_HUNK_CONTENTS
+	];
 
 	pub static ref PATCH: Patch<'static> = {
 		Patch {
@@ -84,6 +103,25 @@ lazy_static! {
 			hunks: Vec::new(),
 		}
 	};
+
+	pub static ref PATCH_NO_NEW_LINES: Patch<'static> = {
+		Patch {
+			change: Change::Modification {
+				modification_type: ModificationType::Edited,
+				old_properties: FileProperties {
+					name: "Test file 2.txt".into(),
+					mode: "100644".into(),
+					index: "60c340c".into(),
+				},
+				new_properties: FileProperties {
+					name: "Test file 2.txt".into(),
+					mode: "100644".into(),
+					index: "ec6c4de".into(),
+				},
+			},
+			hunks: vec![generate_hunk_no_new_lines()],
+		}
+	};
 }
 
 pub fn generate_hunk_1<'a>() -> Hunk<'a> {
@@ -108,4 +146,12 @@ fn generate_patch_data(no_extended_header: bool) -> Vec<u8> {
 			PATCH_DATA_NAMES,
 			&**PATCH_DATA_HUNK_1,
 			&**PATCH_DATA_HUNK_2]
+}
+
+fn generate_hunk_no_new_lines<'a>() -> Hunk<'a> {
+	Hunk {
+		old_file_range: 1..2,
+		new_file_range: 1..2,
+		data: &*PATCH_DATA_NO_NEW_LINES_HUNK_CONTENTS
+	}
 }
